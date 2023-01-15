b7e6e957a8d7b1a7d4580a63855f176557996be4
pvduc <pvduc.dev@gmail.com>
2023-01-15 16:31:25 +0700

    chore: config jest test

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import Button from './Button';
 
 describe('<Button/>', () => {
-  it('should be defined', () => {
+  it('should be defined', function () {
     expect(Button).toBeDefined();
   });
   it('should be render with children', async function () {
     const component = render(
       <Button>
-        Button
+        Submit
       </Button>);
-    expect(component.baseElement).toBeInTheDocument();
+    expect(component.getByRole('button').innerHTML).toBe('Submit');
   });
 });
