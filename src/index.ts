b5666be7ffce461f50de879f71d7484a8718af10
pvduc <pvduc.dev@gmail.com>
2023-02-19 12:01:15 +0700

    chore: update ci workflow

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,4 @@ export { default as Button } from './components/button/Button';
 export { default as Container } from './components/grid-system/Container';
 export { default as Row } from './components/grid-system/Row';
 export { default as  Column } from './components/grid-system/Column';
-export { default as  Flex } from './components/grid-system/Flex';
+// export { default as  Flex } from './components/grid-system/Flex';
